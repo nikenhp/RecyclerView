d6bfa11113d30fbfb3dda5f323d98da7ea045bf5
nikenhp <elfnikenhp@gmail.com>
2018-07-27 20:23:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/myrecyclerview/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.user.myrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvCategory;
+    private ArrayList<President> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rvCategory = (RecyclerView)findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+
+        list = new ArrayList<>();
+        list.addAll(PresidentData.getListData());
+
+        showRecyclerList();
+    }
+
+    private void showRecyclerList(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
+        listPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(listPresidentAdapter);
+    }
+
+    //Menu Pop up
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_list:
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                showRecyclerGrid();
+                break;
+            case R.id.action_cardview:
+                showRecyclerCardView();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Grid
+    private void showRecyclerGrid(){
+        rvCategory.setLayoutManager(new GridLayoutManager(this, 2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
+    }
+
+    //CardView
+    private void showRecyclerCardView(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        CardViewPresidentAdapter cardViewPresidentAdapter = new CardViewPresidentAdapter(this);
+        cardViewPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(cardViewPresidentAdapter);
+    }
+}
